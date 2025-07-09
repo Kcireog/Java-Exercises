552843ec1407931326e0dddb9371470bdd12cb6d
Erick Rodriguez <rodriguezsantiago0230@hotmail.com>
2025-07-08 23:42:03 -0500

    Add 'Codificador De Vocales' problem solution

--- 00-Pruebas-Tecnicas/src/CodificadorDeVocales.java
+++ 00-Pruebas-Tecnicas/src/CodificadorDeVocales.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CodificadorDeVocales {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+        System.out.println("Escriba la frase o palabra que desa codificar: ");
+        String entradaUsuario = leer.nextLine();
+        entradaUsuario = entradaUsuario.toLowerCase();
+
+
+        String salidaUsuario = codificarFrase(entradaUsuario);
+
+        System.out.println("Codificación: " + salidaUsuario);
+    }
+
+    static String codificarFrase(String entrada) {
+        String salida = " ";
+        for (int i = 0; i < entrada.length(); i++) {
+            char letra = entrada.charAt(i);
+            switch (letra) {
+                case 'a':
+                    salida = salida.concat("@");
+                    break;
+                case 'e':
+                    salida = salida.concat("#");
+                    break;
+                case 'i':
+                    salida = salida.concat("$");
+                    break;
+
+                case 'o':
+                    salida = salida.concat("%");
+                    break;
+
+                case 'u':
+                    salida = salida.concat("*");
+                    break;
+                default:
+                    salida = salida.concat(String.valueOf(entrada.charAt(i)));
+
+            }
+        }
+        return salida;
+    }
 }
